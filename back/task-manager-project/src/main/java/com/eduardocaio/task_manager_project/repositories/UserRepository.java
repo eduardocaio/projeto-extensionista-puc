c4b40e9e27ac8b978a43127b11b86fc3ce44ae5d
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2026-04-03 17:03:19 -0300

    Criado interface de acesso a tabela de usuario

---
+++ back/task-manager-project/src/main/java/com/eduardocaio/task_manager_project/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.eduardocaio.task_manager_project.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardocaio.task_manager_project.entities.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long>{
+    Optional<UserEntity> findByUsername(String username);   
+}
