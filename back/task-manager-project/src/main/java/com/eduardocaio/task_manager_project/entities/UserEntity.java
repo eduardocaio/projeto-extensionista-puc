57ad2d75e4cfb00b9ebfb8aae8ae7b5289764662
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2026-04-03 16:53:24 -0300

    Adicionado entidade de usuario

---
+++ back/task-manager-project/src/main/java/com/eduardocaio/task_manager_project/entities/UserEntity.java
@@ -0,0 +1,22 @@
+package com.eduardocaio.task_manager_project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private String username;
+    private String password;
+
+}
