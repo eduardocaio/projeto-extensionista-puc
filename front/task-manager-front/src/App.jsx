a20e979554e21273c832a35d056065663f827367
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2026-04-05 21:40:37 -0300

    Adicionado novas rotas para acesso as novas telas criadas

--- front/task-manager-front/src/App.jsx
+++ front/task-manager-front/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Tasks from './components/Tasks';
+import Tasks from './components/TasksKanban';
 import Sidebar from './components/Sidebar';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './components/Dashboard';
+import UserManagement from "./components/UserManagement";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './App.scss';
+import UserList from "./components/UserList";
+import ProjectsList from "./components/ProjectsList";
+import ProjectManagement from "./components/ProjectManagement";
 
-const Layout = () => (
-  <div className="app-container">
+const Layout = ({ children }) => (
+  <div className="app-container" style={{ display: "flex", height: "100vh" }}>
     <Sidebar />
-    <Tasks />
+    <div className="main-content" style={{ flex: 1, overflowY: "auto" }}>
+      {children}
+    </div>
   </div>
 );
 
@@ -23,13 +30,64 @@ const App = () => {
       <ToastContainer position="bottom-center" autoClose={2500} />
 
       <Routes>
+        {/* Rota pública */}
         <Route path="/login" element={<Login />} />
 
+        {/* Rota privada: Dashboard */}
         <Route 
           path="/" 
           element={
             <PrivateRoute>
-              <Layout />
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </PrivateRoute>
+          } 
+        />
+       
+        <Route 
+          path="/tasks-kanban"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Tasks />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/user-list"
+          element={
+            <PrivateRoute roles={["ADMIN"]}>
+              <UserList />
+            </PrivateRoute>
+          }
+        />  
+
+        <Route
+          path="/user-management"
+          element={
+            <PrivateRoute roles={["ADMIN"]}>
+              <UserManagement />
+            </PrivateRoute>
+          }
+        />  
+
+        <Route 
+          path="/project-list" 
+          element={
+            <PrivateRoute roles={["ADMIN"]}>
+              <ProjectsList />
+            </PrivateRoute>
+          } 
+        />
+
+        <Route 
+          path="/project-management" 
+          element={
+            <PrivateRoute roles={["ADMIN"]}>
+              <ProjectManagement />
             </PrivateRoute>
           } 
         />
